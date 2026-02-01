27004bcd6d1b9e01629ab789c65c54801fb568e7
Pedro <pedrolucascabral4@gmail.com>
2026-02-01 05:17:10 -0300

    feat: test user service

--- src/test/java/com/example/fausto_importados_api/service/UserServiceTest.java
+++ src/test/java/com/example/fausto_importados_api/service/UserServiceTest.java
@@ -1,19 +1,23 @@
 package com.example.fausto_importados_api.service;
 
 import com.example.fausto_importados_api.model.User;
+import com.example.fausto_importados_api.model.enums.Role;
 import com.example.fausto_importados_api.repository.UserRepository;
 import com.example.fausto_importados_api.services.UserService;
+import com.example.fausto_importados_api.services.exception.BusinessException;
+import com.example.fausto_importados_api.services.exception.ResourceNotFoundException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -24,9 +28,10 @@ class UserServiceTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    @Mock
+    @InjectMocks
     private UserService userService;
 
+    @Test
     void shouldReturnUserWhenEmailExists() {
         String email = "admin@example.com";
         User user = new User();
@@ -41,5 +46,60 @@ class UserServiceTest {
         assertEquals(email, result.getEmail());
 
         verify(userRepository).findByEmail(email);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailNotFound() {
+        String email = "admin@example";
+
+        when(userRepository.findByEmail(email))
+                .thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () ->
+                userService.findByEmail(email)
+        );
+
+        verify(userRepository).findByEmail(email);
+    }
+
+    @Test
+    void shouldCreateAdminSuccessfully() {
+        User input = new User();
+        input.setEmail("admin@example.com");
+        input.setPassword("123456");
+
+        when(userRepository.existsByRole(Role.ADMIN))
+                .thenReturn(false);
+
+        when(passwordEncoder.encode("123456"))
+                .thenReturn("senhaCriptografada");
+
+        when(userRepository.save(any(User.class)))
+                .thenAnswer(invocationOnMock -> invocationOnMock.getArguments()[0]);
+
+        User admin = userService.createAdmin(input);
+
+        assertNotNull(admin);
+        assertEquals("admin@example.com", admin.getEmail());
+        assertEquals("senhaCriptografada", admin.getPassword());
+
+        verify(userRepository).existsByRole(Role.ADMIN);
+        verify(passwordEncoder).encode("123456");
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenAdminAlreadyExist() {
+        User input = new User();
+
+        when(userRepository.existsByRole(Role.ADMIN))
+                .thenReturn(true);
+
+        assertThrows(BusinessException.class, () ->
+                userService.createAdmin(input)
+        );
+
+        verify(userRepository).existsByRole(Role.ADMIN);
+        verify(userRepository, never()).save(any());
     }
 }
