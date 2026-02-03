0a02dc8080d2c4045dee18673e6e09aca8354acf
Pedro <pedrolucascabral4@gmail.com>
2026-02-02 22:39:10 -0300

    feat: JwtUserDetailsService

--- src/main/java/com/example/fausto_importados_api/security/jwt/JwtUserDetailsService.java
+++ src/main/java/com/example/fausto_importados_api/security/jwt/JwtUserDetailsService.java
@@ -1,4 +1,27 @@
 package com.example.fausto_importados_api.security.jwt;
 
-public class JwtUserDetailsService {
+import com.example.fausto_importados_api.services.UserService;
+import com.example.fausto_importados_api.services.exception.ResourceNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class JwtUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public JwtUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        try {
+            return userService.findByEmail(email);
+        } catch (ResourceNotFoundException e) {
+            throw new UsernameNotFoundException(
+                    "Admin not found with email: " + email
+            );
+        }
+    }
 }
