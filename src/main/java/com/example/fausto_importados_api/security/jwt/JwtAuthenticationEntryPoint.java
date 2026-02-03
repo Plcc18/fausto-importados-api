26118ea654c57cdf1bf079d3a0bbdc9f75df59a9
Pedro <pedrolucascabral4@gmail.com>
2026-02-02 22:56:06 -0300

    feat: JwtAuthenticationEntryPoint

--- src/main/java/com/example/fausto_importados_api/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/fausto_importados_api/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,42 @@
 package com.example.fausto_importados_api.security.jwt;
 
-public class JwtAuthenticationEntryPoint {
+import java.io.IOException;
+import java.io.Serial;
+import java.io.Serializable;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        response.getWriter().write("""
+            {
+              "status": 401,
+              "error": "Unauthorized",
+              "message": "Authentication is required to access this resource"
+            }
+        """);
+    }
 }
+
+
