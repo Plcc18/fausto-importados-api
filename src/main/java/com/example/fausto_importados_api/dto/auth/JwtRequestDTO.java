e013c6d21c86b414cd8075a9d40e08f08d35fd5c
Pedro <pedrolucascabral4@gmail.com>
2026-02-04 20:22:48 -0300

    fit: record class requestDTO

--- src/main/java/com/example/fausto_importados_api/dto/auth/JwtRequestDTO.java
+++ src/main/java/com/example/fausto_importados_api/dto/auth/JwtRequestDTO.java
@@ -1,20 +1,6 @@
 package com.example.fausto_importados_api.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class JwtRequestDTO {
-
-    @NotBlank(message = "Username is required")
-    private String username;
-
-    @NotBlank(message = "Password is required")
-    private String password;
+public record JwtRequestDTO(String username, String password) {
 }
