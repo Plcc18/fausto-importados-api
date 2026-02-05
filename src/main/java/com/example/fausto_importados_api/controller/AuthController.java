f118310e5da408e2d186edf9e9c635893e0701d0
Pedro <pedrolucascabral4@gmail.com>
2026-02-04 21:55:15 -0300

    feat: AuthController

--- src/main/java/com/example/fausto_importados_api/controller/AuthController.java
+++ src/main/java/com/example/fausto_importados_api/controller/AuthController.java
@@ -1,4 +1,49 @@
 package com.example.fausto_importados_api.controller;
 
+import com.example.fausto_importados_api.dto.auth.JwtRequestDTO;
+import com.example.fausto_importados_api.dto.auth.JwtResponseDTO;
+import com.example.fausto_importados_api.security.jwt.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public AuthController(
+            AuthenticationManager authenticationManager,
+            JwtService jwtService
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponseDTO> login(
+            @RequestBody
+            JwtRequestDTO request
+            ) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.username(),
+                        request.password()
+                )
+        );
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String token = jwtService.generateToken(userDetails);
+
+        return ResponseEntity.ok(new JwtResponseDTO(token));
+    }
 }
