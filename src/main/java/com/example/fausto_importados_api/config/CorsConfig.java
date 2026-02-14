38994ee9eb52af9dfb0454a39e3fb352b0ae43c2
Pedro <pedrolucascabral4@gmail.com>
2026-02-14 20:11:41 -0300

    feat: configure frontend access CORS

---
+++ src/main/java/com/example/fausto_importados_api/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.fausto_importados_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                WebMvcConfigurer.super.addCorsMappings(registry);
+                registry.addMapping("/**")
+                        .allowedOrigins("https://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
